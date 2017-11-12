92274f8fd9f21400b5cd0aaa14d31e766b847a20
Joaquin Melara <melarajoaquin@gmail.com>
2017-11-12 17:31:17 -0500

    deploying website attempt number 1

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
 // CREATING ROUTES
-
+require("./routes/website_routes")(app);
 
 // LISTENING COMPONENT
 app.listen(PORT, function() {
-    console.log("There ain\'t no hollaback girl on PORT: `$PORT`");
+    console.log("There ain\'t no hollaback girl on PORT: " + PORT);
 });
